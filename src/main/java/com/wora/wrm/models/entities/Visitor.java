1056113d84dc921464ef2c697dd989f2819bb153
hamzalamin <hamzalamin80@gmail.com>
2024-11-11 15:22:18 +0100

    Add Visitor entity with basic fields (first name, last name)

---
+++ src/main/java/com/wora/wrm/models/entities/Visitor.java
@@ -0,0 +1,25 @@
+package com.wora.wrm.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "visitor")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visitor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "f_name")
+    private String fName;
+
+    @Column(name = "l_name")
+    private String lName;
+}
