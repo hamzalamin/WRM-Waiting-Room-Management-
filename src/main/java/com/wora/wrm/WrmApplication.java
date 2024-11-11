a25b2e1168db92972131fa951bd15e3f50f58cb4
hamzalamin <hamzalamin80@gmail.com>
2024-11-11 14:26:34 +0100

    Initialized project repository with default setup and dependencies

---
+++ src/main/java/com/wora/wrm/WrmApplication.java
@@ -0,0 +1,13 @@
+package com.wora.wrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wora/wrm/WrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.wrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
