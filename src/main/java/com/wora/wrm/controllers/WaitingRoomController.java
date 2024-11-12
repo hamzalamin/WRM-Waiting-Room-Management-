717784c3b5bd8c3b5ae3b8537e1f6df19a08e2dc
hamzalamin <hamzalamin80@gmail.com>
2024-11-12 10:57:39 +0100

    Added findById endpoint in WaitingRoomController - Created a GET endpoint /api/v1/Waiting-List/{id} to retrieve a WaitingRoom by its ID. - Returns a ResponseEntity with the found WaitingRoomDto and HTTP status 200 (OK).

--- src/main/java/com/wora/wrm/controllers/WaitingRoomController.java
+++ src/main/java/com/wora/wrm/controllers/WaitingRoomController.java
@@ -8,9 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/v1/Waiting-List")
@@ -22,6 +20,11 @@ public class WaitingRoomController {
     @PostMapping("/save")
     public ResponseEntity<WaitingRoomDto> save(@RequestBody @Valid CreateWaitingRoomDto createWaitingRoomDto){
         return new ResponseEntity<>(waitingRoomService.save(createWaitingRoomDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WaitingRoomDto> findById(@PathVariable Long id){
+        return new ResponseEntity<>(waitingRoomService.findById(id), HttpStatus.OK);
     }
 
 }
