64e6f0476cd4bcd8d5896115452576587833cfef
hamzalamin <hamzalamin80@gmail.com>
2024-11-12 16:11:06 +0100

    Implement subscribeVisitor method in VisitService (stub for future logic)

--- src/main/java/com/wora/wrm/services/impl/VisitService.java
+++ src/main/java/com/wora/wrm/services/impl/VisitService.java
@@ -1,7 +1,23 @@
 package com.wora.wrm.services.impl;
 
+import com.wora.wrm.mappers.VisitMapper;
+import com.wora.wrm.models.dtos.visitDto.SubscribeVisitorDto;
+import com.wora.wrm.models.dtos.visitDto.VisitDto;
+import com.wora.wrm.models.entities.Visitor;
+import com.wora.wrm.repositories.VisitRepository;
 import com.wora.wrm.services.interfaces.IVisitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class VisitService implements IVisitService {
 
+    private final VisitRepository visitRepository;
+    private final VisitMapper visitMapper;
+
+    @Override
+    public VisitDto subscribeVisitor(SubscribeVisitorDto subscribeVisitorDto, Long visitorId, Long waitingRoomId) {
+        return null;
+    }
 }
