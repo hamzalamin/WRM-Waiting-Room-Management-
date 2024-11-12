342a9ef885594ad3574124e7016f89b9d19c40af
hamzalamin <hamzalamin80@gmail.com>
2024-11-12 09:02:19 +0100

    Added validation constraints to WaitingRoomDto for input validation
    - Applied @NotNull and @Positive annotations to enforce non-null and positive values for id and date fields.
    - Added @NotNull constraint for capacity to ensure a value is provided.
    - Ensured consistency in validation for better data integrity when creating or updating WaitingRoom instances.

--- src/main/java/com/wora/wrm/models/dtos/WaitingRoomDtos/WaitingRoomDto.java
+++ src/main/java/com/wora/wrm/models/dtos/WaitingRoomDtos/WaitingRoomDto.java
@@ -2,12 +2,14 @@ package com.wora.wrm.models.dtos.WaitingRoomDtos;
 
 import com.wora.wrm.models.enumes.AlgorithmType;
 import com.wora.wrm.models.enumes.WorkMode;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record WaitingRoomDto(
-        Long id,
-        Integer date,
+        @NotNull @Positive Long id,
+        @NotNull @Positive  Integer date,
         AlgorithmType algorithmType,
-        Integer capacity,
-        WorkMode workMode
+        @NotNull Integer capacity,
+        @NotNull WorkMode workMode
 ) {
 }
